959554caa5d2653e55f449da691e83413a2a9e25
Virginia Gilliland <virginiac1418@gmail.com>
2020-12-08 16:09:27 -0500

    jhv

--- assets/js/main.js
+++ assets/js/main.js
@@ -73,7 +73,7 @@ $("#imoverit").on("click", imoverIt)
 
 
 function imoverIt(){
-alert("This game is all about Dark Patterns (a user interface that has been carefully crafted to trick users into doing things, such as buying insurance with their purchase or signing up for recurring bills. Task 1 plays off user expectation that buttons should work how you expect (Answer: click register instead of the x button). Task 2 shows how wording can be deceptive when trying to opt out of a service (Answer: click option 1). Task 3 shows how people are often tricked when trying to sign up for free services (Answer: click audit)");
+alert("This game is all about Dark Patterns (a user interface that has been carefully crafted to trick users into doing things, such as buying insurance with their purchase or signing up for recurring bills). Task 1 plays off user expectation that buttons should work how you expect (Answer: click register instead of the x button). Task 2 shows how wording can be deceptive when trying to opt out of a service (Answer: click option 1). Task 3 shows how people are often tricked when trying to sign up for free services (Answer: click audit)");
 errors = 0;
 }
 
