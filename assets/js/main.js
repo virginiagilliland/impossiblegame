e14b467b688e7052ad36c48be5c801a1293be540
Virginia Gilliland <virginiac1418@gmail.com>
2020-12-08 15:25:08 -0500

    got rid of px

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,24 +11,18 @@ $("#turtleid").on("click",function() {
 */
 
 "use strict";
-// add all the variables ()
 var errors = 0;
 
 var numberErrorsAllowed = 3;
-// list all of the button listeners here
 
 
 
 $(".task1_fakeBtn").on("click",function(){
-	// here update the number of errors
 	errors ++;
 	updatingerrorDisplay();
 });
 
 $(".task1_correctBtn").on("click",function(){
-	//$(this).addClass("checkmarktask1");
-//jQuery(".task1emptybox").addClass("checkmarktask1");
-//jQuery(".task1emptybox").addClass("checkmarktask1");
 jQuery("#task1emptybox").removeClass("blankwhite");
 jQuery("#image1task1").css("z-index", "-999999");
 
@@ -49,7 +43,6 @@ $(".task2_correctBtn").on("click",function(){
 	jQuery("#image2task2").css("z-index", "-999998");
 
 	errors = 0;
-	//move to task 3
 	});
 
 
@@ -91,42 +84,5 @@ function updatingerrorDisplay(){
 		errors = 0
 		$(".errorDisplay").html(errors)
 		imoverIt()
-		/*reset to task 1 image and task 1 buttons*/
 	}
 }
-/*
-function updatingerrorDisplay() {
-		$(".errorDisplay").html(errors)
-}
-
-//if hit button "i'm over it show education pop up, x to close game?"
-//$("#imoverit").on("click", overit);
-
-/*
-
-Task 1: Monkey Image, Have to click on Register instead of 'X' to accomplish tasks
-	if the person clicks on any button other than the register button,
-		add one to their error score
-		if they get to 3 errors end the game & show educational explanation message
-	if they click on the register button
-		cross task one off the list
-		move to task 2
-
-**reset error # before task 2 starts*
-Task 2: Unsubscribe from the email list
-	if they click the correctly worded option
-		cross task off the list
-		move to task 3
-	if they click any option other than the correctly worded one..
-		add one to the error score
-		if they get more than 3 errors end the game & show the educational explanation message
-
-**reset error # before task 3 starts**
-Task 3:
-	if they do it correctly
-		cross task 3 off the list & show congrats/educational message
-	if they do it incorrectly
-		add 1 to error
-		if they get more than 3 errors end the game & show educational explanation message
-
-*/
